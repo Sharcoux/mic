c1d15fe43c6811bf0db6d0bcff16ef418242aebf
François Billioud <fbillioud@cutii.io>
2018-12-05 16:53:25 +0100

    Add eslint
    Signed-off-by: François Billioud <fbillioud@cutii.io>

--- tests/test.js
+++ tests/test.js
@@ -10,45 +10,45 @@ micInputStream.pipe(outputFileStream);
 
 var chunkCounter = 0;
 micInputStream.on('data', function(data) {
-        console.log("Recieved Input Stream of Size %d: %d", data.length, chunkCounter++);
+    console.log('Recieved Input Stream of Size %d: %d', data.length, chunkCounter++);
 });
 
 micInputStream.on('error', function(err) {
-    cosole.log("Error in Input Stream: " + err);
+    console.log('Error in Input Stream: ' + err);
 });
 
 micInputStream.on('startComplete', function() {
-        console.log("Got SIGNAL startComplete");
-        setTimeout(function() {
-                micInstance.pause();
-            }, 5000);
-    });
+    console.log('Got SIGNAL startComplete');
+    setTimeout(function() {
+        micInstance.pause();
+    }, 5000);
+});
     
 micInputStream.on('stopComplete', function() {
-        console.log("Got SIGNAL stopComplete");
-    });
+    console.log('Got SIGNAL stopComplete');
+});
     
 micInputStream.on('pauseComplete', function() {
-        console.log("Got SIGNAL pauseComplete");
-        setTimeout(function() {
-                micInstance.resume();
-            }, 5000);
-    });
+    console.log('Got SIGNAL pauseComplete');
+    setTimeout(function() {
+        micInstance.resume();
+    }, 5000);
+});
 
 micInputStream.on('resumeComplete', function() {
-        console.log("Got SIGNAL resumeComplete");
-        setTimeout(function() {
-                micInstance.stop();
-            }, 5000);
-    });
+    console.log('Got SIGNAL resumeComplete');
+    setTimeout(function() {
+        micInstance.stop();
+    }, 5000);
+});
 
 micInputStream.on('silence', function() {
-        console.log("Got SIGNAL silence");
-    });
+    console.log('Got SIGNAL silence');
+});
 
 micInputStream.on('processExitComplete', function() {
-        console.log("Got SIGNAL processExitComplete");
-    });
+    console.log('Got SIGNAL processExitComplete');
+});
 
 micInstance.start();
 
