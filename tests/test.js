2d404d2d142bbb738a851e25d392f46b4e7b6ca2
François Billioud <fbillioud@cutii.io>
2018-12-05 17:14:18 +0100

    Fix remaining bugs
    Signed-off-by: François Billioud <fbillioud@cutii.io>

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 var mic = require('../index.js');
 var fs = require('fs');
 
-var micInstance = mic({ 'rate': '16000', 'channels': '1', 'debug': false, 'exitOnSilence': 6 });
+var micInstance = mic({ 'rate': '16000', 'channels': '1', 'debug': true, 'exitOnSilence': 3 });
 var micInputStream = micInstance.getAudioStream();
 
 var outputFileStream = fs.WriteStream('output.raw');
@@ -17,39 +17,4 @@ micInputStream.on('error', function(err) {
     console.log('Error in Input Stream: ' + err);
 });
 
-micInputStream.on('startComplete', function() {
-    console.log('Got SIGNAL startComplete');
-    setTimeout(function() {
-        micInstance.pause();
-    }, 5000);
-});
-    
-micInputStream.on('stopComplete', function() {
-    console.log('Got SIGNAL stopComplete');
-});
-    
-micInputStream.on('pauseComplete', function() {
-    console.log('Got SIGNAL pauseComplete');
-    setTimeout(function() {
-        micInstance.resume();
-    }, 5000);
-});
-
-micInputStream.on('resumeComplete', function() {
-    console.log('Got SIGNAL resumeComplete');
-    setTimeout(function() {
-        micInstance.stop();
-    }, 5000);
-});
-
-micInputStream.on('silence', function() {
-    console.log('Got SIGNAL silence');
-});
-
-micInputStream.on('processExitComplete', function() {
-    console.log('Got SIGNAL processExitComplete');
-});
-
 micInstance.start();
-
-
