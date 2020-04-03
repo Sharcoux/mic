1bec71196ce60de96cecc9d400709a5be3db0a9b
bfamchon <bfamchon@cutii.io>
2020-04-03 16:17:03 +0200

    :sparkles: add a hard kill feature for mic process and stopCompleted event for soft kill

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,18 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "es6": true
+    env: {
+        node: true,
+        es6: true,
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2015,
-        "sourceType": "module"
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 2015,
+        sourceType: 'module',
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    rules: {
+        'no-console': 'off',
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+    },
 };
